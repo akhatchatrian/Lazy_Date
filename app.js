dccf3f6019de7ec584c46a902bff4b3a86d4beb3
ehecker <ezhecker@gmail.com>
2020-03-16 15:55:05 -0700

    Implement backend user authentication

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const db = require('./config/keys').mongoURI;
+const bodyParser = require('body-parser');
+const passport = require('passport');
+
+
+const users = require("./routes/api/users");
 
 mongoose
   .connect(db, { useNewUrlParser: true })
@@ -9,6 +14,15 @@ mongoose
   .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello World"));
+
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/api/users", users);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
