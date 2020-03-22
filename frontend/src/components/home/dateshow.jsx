6a52cd6f4fccd34a458fc9f782de8ab99f84d04b
akhatchatrian <58192944+akhatchatrian@users.noreply.github.com>
2020-03-22 15:10:22 -0700

    User update (#4)
    Created `updateUser` route and wired it to the `dateshow` component.

--- frontend/src/components/home/dateshow.jsx
+++ frontend/src/components/home/dateshow.jsx
@@ -1,22 +1,14 @@
 import React from "react";
 import DateCarousel from './date_carousel.jsx';
-import { Link } from "react-router-dom";
-
 
 class DateShow extends React.Component {
 
     constructor(props) {
         super(props)
         this.formData = this.props.location.state; //Object from DateSearchForm containing yelpInfo and collectionInfo keys.
+
     }
 
-    componentDidMount() {
-        this.props.yelpSearch(this.formData.yelpInfo)
-    }
-
-    componentWillUpdate(){
-        // this.props.yelpSearch(this.props.session.sessionDate)
-    }
 
     deepMind() {
         const results = this.props.yelpData.businesses
@@ -26,34 +18,35 @@ class DateShow extends React.Component {
             let smartRating2 = (venue2.rating * venue2.review_count + 6) / (venue2.review_count + 2);
             return smartRating2 - smartRating1;
         })
+        
+        const userData = {
+            currentUser: this.props.currentUser.id,
+            dateEvents: results.slice(0, 5)
+        }
+        
+        this.props.updateUser(userData)
 
         return results.slice(0, 5);
     }
 
     render() {
-
         if (!this.props.yelpData || Object.values(this.props.yelpData).length === 0) {
             return null
         }
 
-        const businesses = this.props.yelpData.businesses;
-       
-
-        return (
-          <div className="date-show-container">
-            {businesses.length === 0 ? (
-              <div className='no-results-container'>
-                  <p>Sorry, date could not be generated with your selected options</p>
-                  <p>Please try again with different parameters.</p>
-                  <a href="#/date"><button class="create-date-btn">Create a new Date</button></a>
-              </div>
-            ) : (
-              <DateCarousel dates={businesses} />
-            )}
-
-            {/* {this.deepMind()} */}
-          </div>
-        );
+        return(
+            <div className='date-show-container'>
+                <div>Here be the Date Show page</div>
+                <DateCarousel 
+                        currentUser={this.props.currentUser}
+                        userUpdate={this.props.userUpdate}
+                        formData={this.formData}
+                        createDateCollection={this.props.createDateCollection} 
+                        yelpData={this.props.yelpData} 
+                        results={this.deepMind()} 
+                    /> 
+            </div>
+        )
     }
 
 }
