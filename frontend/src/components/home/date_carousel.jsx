216de292726b33fc6ec5545eac5a98ebc20bc88d
echu18 <elizabethchu18@gmail.com>
2020-03-22 02:38:03 -0700

    push

--- frontend/src/components/home/date_carousel.jsx
+++ frontend/src/components/home/date_carousel.jsx
@@ -71,8 +71,8 @@ class DateCarousel extends React.Component {
         this.changeMainDate(newDateIdx);
     }
 
-    getStars(rating, reviewCount) {
-        if (!rating || !reviewCount) return null;
+    getStars(rating) {
+        if (!rating) return null;
 
         const star = <i className="fas fa-star"></i>;
         const halfstar = <i className="fas fa-star-half"></i>;
@@ -84,7 +84,7 @@ class DateCarousel extends React.Component {
 
         if (!Number.isInteger(rating)) stars.push(halfstar);
         
-        return (<div className='star-rating'>{stars} ({reviewCount})</div>)
+        return stars;
     }
 
     getDollars(price) {
@@ -127,70 +127,76 @@ class DateCarousel extends React.Component {
 
         return (
           <div className="date-carousel">
-            <div className="date-main-container">
-              <div className="date-main-header">
-                <h3>{currentDate.name}</h3>
-                <p>{currentDate.categories[0].title}</p>
-                {this.getStars(currentDate.rating, currentDate.review_count)}
-              </div>
-              <div className="date-main-img-container">
-                <div className="arrow-icons">
-                  <div
-                    className="left-arrow-icon"
-                    onClick={this.handlePrevious}
-                  >
-                    <i className="fas fa-chevron-left"></i>
-                    {/* <button>Previous</button> */}
-                  </div>
 
-                  <div className="right-arrow-icon" onClick={this.handleNext}>
-                    <i className="fas fa-chevron-right"></i>
-                    {/* <button>Next</button> */}
-                  </div>
-                </div>
-
-                <div className="date-main-img">
-                  <img src={currentDate.image_url} />
-                </div>
-              </div>
-
-              <div className="date-detailed-info">
-                <h3 className="detail-name">{currentDate.name}</h3>
-                <p className="detail-cats">{catTitles}</p>
-                <p className="detail-price">{this.getDollars(currentDate.price)}</p>
-                <p className="detail-rating">{currentDate.rating}</p>
-                <p className="detail-phone">{currentDate.display_phone}</p>
-
-                <div className="date-address">
-                  <p>{currentDate.location.address1}</p>
-                  <p>{currentDate.location.city}</p>
-                  <p>{currentDate.location.zip_code}</p>
-                </div>
-              </div>
-            </div>
-
-            <div className="date-list-sidebar">
-                <h3>Your Results:</h3>
-              {dates.map((date, idx) => {
-                return (
-                  <div
-                    className="date-listing"
-                    id={idx}
-                    key={idx}
-                    onClick={e => this.handleClick(e)}
-                    onMouseEnter={e => this.handleMouseEnter(e)}
-                    onMouseLeave={e => this.handleMouseLeave(e)}
-                  >
-                    <img className="date-img" src={date.image_url} />
-                    <div className="date-info-blurb">
-                      <h3>{date.name}</h3>
-                      <p>{date.categories[0].title}</p>
+                <div className="date-main-container">
+                    <div className="date-main-header">
+                        <h3>{currentDate.name}</h3>
+                        <p>{currentDate.categories[0].title}</p>
+                        {currentDate.rating ?
+                            (<div className='star-rating'> {this.getStars(currentDate.rating)} ({currentDate.review_count})</div>)
+                        : null }
                     </div>
-                  </div>
-                );
-              })}
-            </div>
-          </div>
+
+                    <div className="date-main-img-container">
+                        <div className="arrow-icons">
+                            <div
+                                className="left-arrow-icon"
+                                onClick={this.handlePrevious}>
+                                <i className="fas fa-chevron-left"></i>
+                                {/* <button>Previous</button> */}
+                            </div>
+
+                            <div className="right-arrow-icon" onClick={this.handleNext}>
+                                <i className="fas fa-chevron-right"></i>
+                                {/* <button>Next</button> */}
+                            </div>
+                        </div>
+
+                        <div className="date-main-img">
+                            <img src={currentDate.image_url} />
+                        </div>
+                    </div>
+
+                    <div className="date-detailed-info">
+                        <h3 className="detail-name">{currentDate.name}</h3>
+                        <p className="detail-cats">{catTitles}</p>
+                        {currentDate.rating ?
+                            (<div className='detail-rating'> {this.getStars(currentDate.rating)}</div>)
+                            : null }
+                        <p className="detail-price">{this.getDollars(currentDate.price)}</p>
+                        <p className="detail-phone">{currentDate.display_phone}</p>
+
+                        <div className="date-address">
+                            <h3>Location:</h3>
+                            <p>{currentDate.location.address1}</p>
+                            <p>{currentDate.location.city}</p>
+                            <p>{currentDate.location.zip_code}</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="date-list-sidebar">
+                    <h3>Your Results:</h3>
+                    {dates.map((date, idx) => {
+                        return (
+                        <div
+                            className="date-listing"
+                            id={idx}
+                            key={idx}
+                            onClick={e => this.handleClick(e)}
+                            onMouseEnter={e => this.handleMouseEnter(e)}
+                            onMouseLeave={e => this.handleMouseLeave(e)}
+                        >
+                            <img className="date-img" src={date.image_url} />
+                            <div className="date-info-blurb">
+                            <h3>{date.name}</h3>
+                            <p>{date.categories[0].title}</p>
+                            </div>
+                        </div>
+                        );
+                    })}
+                </div>
+        </div>
         );
     }
 }
